5ef318a6c8c1d2f4d00138fb23fb0cc4fca48f74
dgarcez- <job.danielvicenteg@gmail.com>
2026-06-05 19:52:27 +0000

    finished

--- cpp09/ex00/srcs/main.cpp
+++ cpp09/ex00/srcs/main.cpp
@@ -3,15 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dgarcez- < dgarcez-@student.42lisboa.com > +#+  +:+       +#+        */
+/*   By: dgarcez- <dgarcez-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/15 16:22:57 by dgarcez-          #+#    #+#             */
-/*   Updated: 2026/06/02 18:22:02 by dgarcez-         ###   ########.fr       */
+/*   Updated: 2026/06/05 19:48:05 by dgarcez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/BitcoinExchange.hpp"
-
 
 void	print_database(std::map<std::string, float> &db)
 {
@@ -70,37 +69,68 @@ bool	parse_date(t_date &d)
 	return (true);
 }
 
-bool	parse_btc(std::string &date, std::string &value, bool file)
+bool	input_parse(std::string &date, std::string &value)
 {
-	t_date d;
-	if (date.empty() == true || value.empty() == true)
+	if (strtod(value.c_str(), NULL) > 1000)
+	{
+		std::cerr << "Error: No values above 1000" << std::endl;
 		return (false);
-	if (file == DB && (value.find_first_not_of("1234567890.") != value.npos || std::count(value.begin(), value.end(), '.') > 1))
-		return (false);
-	if (file == INPUT && (value.find_first_not_of("1234567890. ") != value.npos || std::count(value.begin(), value.end(), '.') > 1))
+	}
+	if ((value.find_first_not_of("1234567890. ") != value.npos || std::count(value.begin(), value.end(), '.') > 1))
 	{
 		std::cerr << "Error: Invalid value" << std::endl;
 		return (false);
 	}
-	if (file == INPUT && (std::count(value.begin(), value.end(), ' ') != 1 || std::count(date.begin(), date.end(), ' ') != 1))
-	{
-		std::cerr << "Error: Input must have 1 space between pipe \"xxxx-xx-xx | x\"" << std::endl;
-		return (false);
-	}
-	if (value == ".")
-		return (false);
-	if (file == DB && (date.find_first_not_of("1234567890-") != date.npos || std::count(date.begin(), date.end(), '-') > 2))
-		return (false);
-	if (file == INPUT && (date.find_first_not_of("1234567890- ") != date.npos || std::count(date.begin(), date.end(), '-') > 2))
+	if ((date.find_first_not_of("1234567890- ") != date.npos || std::count(date.begin(), date.end(), '-') > 2))
 	{
 		std::cerr << "Error: Invalid date" << std::endl;
 		return (false);
 	}
+	if ((std::count(value.begin(), value.end(), ' ') != 1 || std::count(date.begin(), date.end(), ' ') != 1 ||(*value.begin()) != ' ' || (*date.rbegin()) != ' '))
+	{
+		std::cerr << "Error: Input must have 1 space between pipe \"xxxx-xx-xx | x\"" << std::endl;
+		return (false);
+	}
+	return (true);
+}
+
+bool	parse_btc(std::string &date, std::string &value, bool file)
+{
+	t_date d;
+	std::cout << "date = " << date << "$ value = " << value << "$" << std::endl;
+	if (date.empty() == true || value.empty() == true)
+	{
+		if (file == INPUT)
+			std::cerr << "Error: Empty values" << std::endl;
+		return (false);
+	}
+	if (file == DB && (value.find_first_not_of("1234567890.") != value.npos || std::count(value.begin(), value.end(), '.') > 1))
+		return (false);
+	if (file == DB && (date.find_first_not_of("1234567890-") != date.npos || std::count(date.begin(), date.end(), '-') > 2))
+		return (false);
+	if (strtod(value.c_str(), NULL) < 0)
+	{
+		if (file == INPUT)
+			std::cerr << "Error: Not a positive value" << std::endl;
+		return (false);
+	}
+	if (value.size() <= 1 || date.size() < 10)
+	{
+		std::cerr << "Error: Empty value" << std::endl;
+		return (false);
+	}
+	if (file == INPUT && input_parse(date, value) == false)
+		return (false);
+	if (value == ".")
+		return (false);
 	d.year = strtod(date.substr(0, date.find('-')).c_str(), NULL);
 	d.month = strtod(date.substr(date.find('-') + 1, date.find('-')).c_str(), NULL);
 	d.day = strtod(date.substr(date.find('-') + 4).c_str(), NULL);
 	if (parse_date(d) == false)
+	{
+		std::cerr << "Error: Invalid date" << std::endl;	
 		return (false);
+	}
 	return (true);
 }
 
@@ -124,16 +154,8 @@ bool make_database(std::map<std::string, float> &db, std::ifstream &data)
 	{
 		date = line.substr(0, line.find(','));
 		value = line.substr(line.find(',') + 1);
-		std::cout << date << "$ ";
-		std::cout << value << "$";
 		if (parse_btc(date, value, DB) == true)
-		{
-			std::cout << "-> Insterted!!";
 			db.insert(std::make_pair(date, strtod(value.c_str(), NULL)));
-		}
-		else
-			std::cout << "-> Error!!";
-		std::cout << std::endl; 
 	}
 	return (true);
 }
@@ -145,16 +167,16 @@ bool	parse(std::ifstream &input_file, std::map<std::string, float> &db)
 		return (false);
 	if (make_database(db, data) == false)
 		return (false);
-	// print_database(db);
+	print_database(db);
 	return (true);
 }
 
 void	search_btc(std::ifstream &input_file, std::map<std::string, float> &db)
 {
-	(void)db;
-	std::string line;
-	std::string		date;
-	std::string		value;
+	std::map<std::string, float>::iterator it;
+	std::string	line;
+	std::string	date;
+	std::string	value;
 	std::getline(input_file, line);
 	if (line.empty())
 	{
@@ -170,15 +192,17 @@ void	search_btc(std::ifstream &input_file, std::map<std::string, float> &db)
 	{
 		date = line.substr(0, line.find('|'));
 		value = line.substr(line.find('|') + 1);
-		std::cout << "date: " << date << "$ ";
-		std::cout << "value: " << value << "$ -> ";
 		if (parse_btc(date, value, INPUT) == true)
 		{
-			std::cout << " yep cock " << std::endl;
+			it = db.upper_bound(date);
+			if (it != db.begin())
+			{
+				it--;
+				std::cout << it->first << " =>" << value << " = " << it->second * strtod(value.c_str(), NULL) << std::endl; 	
+			}
+			else
+				std::cerr << date << " >= Error: Nothing found in database" << std::endl;
 		}
-		else
-			std::cout << " Nooo !";
-		std::cout << std::endl; 
 	}
 }
 
